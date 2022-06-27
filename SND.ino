c3b5e378f5642dfbf5d14b7ff16f587f21626d15
Jonas <jonas.schoenerstedt@gmail.com>
2022-06-27 10:38:29 +0200

    Anpassung von Max

--- SND.ino
+++ SND.ino
@@ -1,6 +1,8 @@
 int ledPin = 14;
 int SND_Pin = 6;
 int zaehler = 0;
+int an = 1;
+int gezaehlt = 0;
 
 void setup() {
   pinMode(ledPin, OUTPUT);
@@ -8,17 +10,25 @@ void setup() {
 }
 
 void loop() {
-  if (digitalRead(SND_Pin))
+
+  if(!digitalRead(SND_Pin) && !gezaehlt)
+  {
+    gezaehlt = 1;
+  }
+  if (digitalRead(SND_Pin) && gezaehlt)
   {
     zaehler++;
-    if(zaehler >= 100)
+    gezaehlt = 0;
+    if((zaehler >= 100))
     {
       zaehler = 0;
+      an = 1;
       digitalWrite(ledPin, LOW);
     }
-    else if(zaehler >= 50)
+    else if((zaehler >= 50) && an)
     {
       digitalWrite(ledPin, HIGH);
+      an = 0;
     }
   }
 
