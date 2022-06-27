59a92d28c838b0d7ba32c02072960ac1c5d9d989
Jonas <jonas.schoenerstedt@gmail.com>
2022-06-27 10:28:54 +0200

    noch nicht fertig

--- SND.ino
+++ SND.ino
@@ -1,6 +1,7 @@
 int ledPin = 14;
-int SND_Pin = 0;
-int speicher = 0;
+int SND_Pin = 6;
+int zaehler = 0;
+int an = 1;
 
 void setup() {
   pinMode(ledPin, OUTPUT);
@@ -8,6 +9,20 @@ void setup() {
 }
 
 void loop() {
-  speicher = digitalRead(SND_Pin);
-  digitalWrite(ledPin, speicher);
+  if (digitalRead(SND_Pin))
+  {
+    zaehler++;
+    if((zaehler >= 50) &&  an)
+    {
+      digitalWrite(ledPin, HIGH);
+    }
+    if(zaehler >= 100)
+    {
+      an = 0;
+      zaehler = 0;
+      digitalWrite(ledPin, LOW);
+    }
+  }
+
+  digitalWrite(ledPin, HIGH);
 }
