95edeacfc74317e563f818d37e2e770b6ed14324
Jonas <jonas.schoenerstedt@gmail.com>
2022-06-27 11:11:01 +0200

    Zündwinkel update

--- Zündwinkel.ino
+++ Zündwinkel.ino
@@ -1,7 +1,5 @@
 int Triac_Pin = 3;
 int SND_Pin = 6;
-int zaehler = 0;
-int an = 1;
 int gezaehlt = 0;
 
 void setup() {
@@ -17,20 +15,10 @@ void loop() {
   }
   if (digitalRead(SND_Pin) && gezaehlt)
   {
-    zaehler++;
     gezaehlt = 0;
-    if((zaehler >= 100))
-    {
-      zaehler = 0;
-      an = 1;
-      delayMicroseconds(10);
-      digitalWrite(Triac_Pin, LOW);
-    }
-    else if((zaehler >= 50) && an)
-    {
+    delayMicroseconds(294);
+    digitalWrite(Triac_Pin, HIGH);
     delayMicroseconds(10);
-      digitalWrite(Triac_Pin, HIGH);
-      an = 0;
-    }
+    digitalWrite(Triac_Pin, LOW);
   }
 }
